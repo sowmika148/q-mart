00701043cc646db6b8e9006f3aac82d8948478d3
sowmika <sowmika148@gmail.com>
2020-05-13 16:14:17 +0530

    Fix header and change prices of items

--- src/Utils/ShoppingList.js
+++ src/Utils/ShoppingList.js
@@ -3,31 +3,31 @@ var items = {
     id: "1",
     name: "Kurkure",
     path: "/images/Kurkure.jpg",
-    price: "Rs.10"
+    price: "Rs.20"
   },
   "2": {
     id: "2",
     name: "Maggi",
     path: "../../../images/Maggi.jpg",
-    price: "Rs.10"
+    price: "Rs.15"
   },
   "3": {
     id: "3",
     name: "Mango Pickle",
     path: "/images/Pickle.jpg",
-    price: "Rs.10"
+    price: "Rs.100"
   },
   "4": {
     id: "4",
     name: "Amul Butter",
     path: "/images/Amul.jpg",
-    price: "Rs.10"
+    price: "Rs.75"
   },
   "5": {
     id: "5",
     name: "Nutella",
     path: "/images/Nutella.jpg",
-    price: "Rs.10"
+    price: "Rs.400"
   }
 };
 
